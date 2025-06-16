e61ae3c9c646f03f2b88e434744d4a7c67944165
MatthewKainde <s22310300@student.unklab.ac.id>
2025-06-16 11:33:32 +0800

    Exercise#7

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ import Exercise3 from './Exercise3';
 import Flexbox from './Flexbox';
 import Exercise5 from './Exercise5';
 import Exercise6 from './Exercise6';
+import Exercise7 from './Exercise7';
 import Login from './Login';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Exercise6);
+AppRegistry.registerComponent(appName, () => Exercise7);
